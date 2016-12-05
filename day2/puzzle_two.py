f8449fc011d7e3134fa4a4e5f785074b99608d15
Benjamin J Freeman <bfreema8@kent.edu>
2016-12-05 07:31:27 -0500

    almost fixed the unit tests for part 2 day 2

--- day2/puzzle_two.py
+++ day2/puzzle_two.py
@@ -8,15 +8,21 @@ class Keypad(object):
 
     def __init__(self):
         """ let's define the keypad state """
-        self._row_one   = [1, 2, 3]
-        self._row_two   = [4, 5, 6]
-        self._row_three = [7, 8, 9]
-        self.keypad = [self._row_one, self._row_two, self._row_three]
+        self._row_one   = [0, 0, 1, 0, 0]
+        self._row_two   = [0, 2, 3, 4, 0]
+        self._row_three = [5, 6, 7, 8, 9]
+        self._row_four  = [0, 'A', 'B', 'C', 0]
+        self._row_five  = [0, 0, 'D', 0, 0]
+        self.keypad = [self._row_one,
+                       self._row_two,
+                       self._row_three,
+                       self._row_four,
+                       self._row_five,]
 
         # the keypad starts at number 5, row 2, column 2
         # but lists start counting at 0, so row 1, column 1
-        self.row = 1
-        self.column = 1
+        self.row = 2
+        self.column = 0
 
     def get_key(self):
         """ get the current key based on the row / column """
@@ -24,19 +30,20 @@ class Keypad(object):
 
     def move_up(self):
         """ move up on the keypad """
-        if self.row > 0:
+        if self.row > 0 and self.keypad[self.row - 1][self.column] != 0:
             self.row = self.row - 1
 
     def move_down(self):
         """ move down on the keypad """
         # make sure we don't go past the keypad length
         # just in case it might change
-        if self.row < len(self._row_one) - 1:
+        if self.row < len(self.keypad) - 1 and \
+        self.keypad[self.row + 1][self.column] != 0:
             self.row = self.row + 1
 
     def move_left(self):
         """ move left on the keypad """
-        if self.column > 0:
+        if self.column > 0 and self.keypad[self.row][self.column - 1] != 0:
             self.column = self.column - 1
 
     def move_right(self):
--- day2/test_puzzle_two.py
+++ day2/test_puzzle_two.py
@@ -18,31 +18,45 @@ class TestKeypad(unittest.TestCase):
         """ test the default keypad """
 
         # make sure we start on the middle key, 5
-        self.assertTrue(self.keypad.row == 1)
-        self.assertTrue(self.keypad.column == 1)
+        self.assertTrue(self.keypad.row == 2)
+        self.assertTrue(self.keypad.column == 0)
 
     def test_get_key(self):
         """ make sure that 'get_key' returns the expected value """
-        # check the midle middle
+        # check the midle left
         self.assertEqual(self.keypad.get_key(), 5)
 
-        # check the top left
+        # check the top middle
         self.keypad.row = 0
-        self.keypad.column = 0
+        self.keypad.column = 2
         self.assertEqual(self.keypad.get_key(), 1)
 
-        # check the bottom right
+        # check the middle right
         self.keypad.row = 2
-        self.keypad.column = 2
+        self.keypad.column = 4
         self.assertEqual(self.keypad.get_key(), 9)
+
+        # check the bottom middle
+        self.keypad.row = 4
+        self.keypad.column = 2
+        self.assertEqual(self.keypad.get_key(), 'D')
+
 
     def test_move_up(self):
         """ make sure we move up correctly"""
-        # make sure we move up from 1 to 0
+        # make sure we can't move up from 2,0 to 1,0
         self.keypad.move_up()
-        self.assertTrue(self.keypad.row == 0)
+        self.assertTrue(self.keypad.row == 2)
 
-        # make sure that, when we can't move up, we stay at 0
+        # make sure that we can move up from 3,1 to 2,1
+        self.keypad.row = 3
+        self.keypad.column = 1
+        self.keypad.move_up()
+        self.assertTrue(self.keypad.row == 2)
+
+        # make sure we won't go out of bounds from 0,2
+        self.keypad.row = 0
+        self.keypad.correctly = 2
         self.keypad.move_up()
         self.assertTrue(self.keypad.row == 0)
 
@@ -52,19 +66,33 @@ class TestKeypad(unittest.TestCase):
         self.keypad.move_down()
         self.assertTrue(self.keypad.row == 2)
 
-        # make sure that, when we can't move down, we stay at 2
+        # make sure that we can move down from 2,1 to 3,1
+        self.keypad.row = 2
+        self.keypad.column = 1
         self.keypad.move_down()
-        self.assertTrue(self.keypad.row == 2)
+        self.assertTrue(self.keypad.row == 3)
+
+        # make sure that we can't go out of bounds from 4,2
+        self.keypad.row = 4
+        self.keypad.column = 2
+        self.keypad.move_down()
+        self.assertTrue(self.keypad.row == 4)
 
     def test_move_left(self):
         """ make sure we move left correctly """
-        # make sure we move left from 1 to 0
+        # make sure that we can't go out of bounds from 2,0
         self.keypad.move_left()
         self.assertTrue(self.keypad.column == 0)
 
+        # make sure that we move left from 1,2 to 1,1
+        self.keypad.row = 1
+        self.keypad.column = 2
+        self.keypad.move_left()
+        self.assertTrue(self.keypad.column == 1)
+
         # make sure that, when we can't move left, we stay at 0
         self.keypad.move_left()
-        self.assertTrue(self.keypad.column == 0)
+        self.assertTrue(self.keypad.column == 1)
 
     def test_move_right(self):
         """ make sure we move down correctly """
